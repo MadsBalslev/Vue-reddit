d72f5d1ba4655a077c6760e9c9069e29cde7a1eb
Mads Balslev <madspbalslev@gmail.com>
2020-09-27 15:56:50 +0200

    Initial commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Reddit</a>
+      <form class="right">
+        <div class="input-field">
+          <input class="autocomplete" id="autocomplete-input" type="search" required placeholder="Search">
+          <label class="label-icon" for="autocomplete-input"><i class="material-icons">search</i></label>
+          <i class="material-icons">close</i>
+        </div>
+      </form>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+</style>
